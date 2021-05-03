ad23390508bc41de92c5ffc2b214e34376590ca0
anjali <anjali@code.org>
2021-05-03 14:52:55 -0700

    consolidate body

--- apps/src/p5lab/AnimationPicker/AnimationPicker.jsx
+++ apps/src/p5lab/AnimationPicker/AnimationPicker.jsx
@@ -1,9 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {createUuid} from '@cdo/apps/utils';
 import {connect} from 'react-redux';
-import BaseDialog from '@cdo/apps/templates/BaseDialog.jsx';
+import {createUuid} from '@cdo/apps/utils';
+import color from '@cdo/apps/util/color';
 var msg = require('@cdo/locale');
+import BaseDialog from '@cdo/apps/templates/BaseDialog.jsx';
+import SearchBar from '@cdo/apps/templates/SearchBar';
+import HiddenUploader from '@cdo/apps/code-studio/components/HiddenUploader';
+import {
+  searchAssets,
+  filterOutBackgrounds
+} from '@cdo/apps/code-studio/assets/searchAssets';
+
 import styles from './styles';
 import {
   hide,
@@ -13,12 +21,39 @@ import {
   handleUploadComplete,
   handleUploadError
 } from './animationPickerModule';
-import AnimationPickerBody from './AnimationPickerBody.jsx';
-import HiddenUploader from '@cdo/apps/code-studio/components/HiddenUploader';
+import ScrollableList from '../AnimationTab/ScrollableList.jsx';
+import AnimationPickerListItem from './AnimationPickerListItem.jsx';
 
 // Some operating systems round their file sizes, so max size is 101KB even
 // though our error message says 100KB, to help users avoid confusion.
 const MAX_UPLOAD_SIZE = 101000;
+
+const MAX_SEARCH_RESULTS = 40;
+const MAX_HEIGHT = 460;
+
+const animationPickerStyles = {
+  allAnimations: {
+    color: color.purple,
+    fontFamily: "'Gotham 7r', sans-serif",
+    cursor: 'pointer'
+  },
+  breadCrumbs: {
+    margin: '8px 0',
+    fontSize: 14,
+    display: 'inline-block'
+  },
+  pagination: {
+    float: 'right',
+    display: 'inline',
+    marginTop: 10
+  },
+  emptyResults: {
+    paddingBottom: 10
+  },
+  navigation: {
+    minHeight: 30
+  }
+};
 
 /**
  * Dialog used for finding/selecting/uploading one or more assets to add to a
@@ -60,7 +95,142 @@ class AnimationPicker extends React.Component {
     playAnimations: PropTypes.bool.isRequired
   };
 
+  state = {
+    searchQuery: '',
+    categoryQuery: '',
+    currentPage: 0
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.defaultQuery !== nextProps.defaultQuery) {
+      const currentPage = 0;
+      const {results, pageCount} = this.searchAssetsWrapper(
+        currentPage,
+        nextProps.defaultQuery
+      );
+      let nextQuery = nextProps.defaultQuery || {
+        categoryQuery: '',
+        searchQuery: ''
+      };
+      this.setState({
+        ...nextQuery,
+        currentPage,
+        results,
+        pageCount
+      });
+    }
+  }
+
+  searchAssetsWrapper = (page, config = {}) => {
+    let {searchQuery, categoryQuery, libraryManifest} = config;
+
+    // Set defaults if any config values are undefined.
+    if (searchQuery === undefined) {
+      searchQuery = this.state.searchQuery;
+    }
+    if (categoryQuery === undefined) {
+      categoryQuery = this.state.categoryQuery;
+    }
+    if (libraryManifest === undefined) {
+      libraryManifest = this.props.libraryManifest;
+    }
+
+    return searchAssets(
+      searchQuery,
+      categoryQuery,
+      libraryManifest,
+      page,
+      MAX_SEARCH_RESULTS
+    );
+  };
+
+  handleScroll = event => {
+    const scrollWindow = event.target;
+    const {currentPage, results, pageCount} = this.state;
+    const nextPage = currentPage + 1;
+    if (
+      scrollWindow.scrollTop + MAX_HEIGHT >= scrollWindow.scrollHeight * 0.9 &&
+      (!pageCount || nextPage <= pageCount)
+    ) {
+      let {results: newResults, pageCount} = this.searchAssetsWrapper(nextPage);
+      if (this.props.hideBackgrounds) {
+        newResults = filterOutBackgrounds(newResults);
+      }
+
+      this.setState({
+        results: [...(results || []), ...newResults],
+        currentPage: nextPage,
+        pageCount
+      });
+    }
+  };
+
+  onSearchQueryChange = searchQuery => {
+    const currentPage = 0;
+    let {results, pageCount} = this.searchAssetsWrapper(currentPage, {
+      searchQuery
+    });
+    if (this.props.hideBackgrounds) {
+      results = filterOutBackgrounds(results);
+    }
+    this.setState({searchQuery, currentPage, results, pageCount});
+  };
+
+  onCategoryChange = event => {
+    const categoryQuery = event.target.className;
+    const currentPage = 0;
+    let {results, pageCount} = this.searchAssetsWrapper(currentPage, {
+      categoryQuery
+    });
+    if (this.props.hideBackgrounds) {
+      results = filterOutBackgrounds(results);
+    }
+    this.setState({categoryQuery, currentPage, results, pageCount});
+  };
+
+  onClearCategories = () => {
+    this.setState({
+      categoryQuery: '',
+      searchQuery: '',
+      currentPage: 0,
+      results: [],
+      pageCount: 0
+    });
+  };
+
   onUploadClick = () => this.refs.uploader.openFileChooser();
+
+  animationCategoriesRendering() {
+    let categories = Object.keys(this.props.libraryManifest.categories || []);
+    if (this.props.hideBackgrounds) {
+      categories = categories.filter(category => category !== 'backgrounds');
+    }
+    categories.push('all');
+    return categories.map(category => (
+      <AnimationPickerListItem
+        key={category}
+        label={
+          msg[`animationCategory_${category}`]
+            ? msg[`animationCategory_${category}`]()
+            : category
+        }
+        category={category}
+        onClick={this.onCategoryChange}
+      />
+    ));
+  }
+
+  animationItemsRendering(animations) {
+    return animations.map(animationProps => (
+      <AnimationPickerListItem
+        key={animationProps.sourceUrl}
+        label={this.props.hideAnimationNames ? undefined : animationProps.name}
+        animationProps={animationProps}
+        onClick={this.props.onPickLibraryAnimation.bind(this, animationProps)}
+        playAnimations={this.props.playAnimations}
+      />
+    ));
+  }
 
   renderVisibleBody() {
     if (this.props.uploadError) {
@@ -69,22 +239,72 @@ class AnimationPicker extends React.Component {
       );
     } else if (this.props.uploadInProgress) {
       return <h1 style={styles.title}>{msg.animationPicker_uploading()}</h1>;
+    } else if (!this.props.libraryManifest) {
+      return <div>{msg.loading()}</div>;
     }
+    const {searchQuery, categoryQuery, results} = this.state;
+    const {hideUploadOption, is13Plus} = this.props;
     return (
-      <AnimationPickerBody
-        is13Plus={this.props.is13Plus}
-        onDrawYourOwnClick={this.props.onPickNewAnimation}
-        onPickLibraryAnimation={this.props.onPickLibraryAnimation}
-        onUploadClick={this.onUploadClick}
-        playAnimations={this.props.playAnimations}
-        libraryManifest={this.props.libraryManifest}
-        hideUploadOption={this.props.hideUploadOption}
-        hideAnimationNames={this.props.hideAnimationNames}
-        navigable={this.props.navigable}
-        defaultQuery={this.props.defaultQuery}
-        hideBackgrounds={this.props.hideBackgrounds}
-        canDraw={this.props.canDraw}
-      />
+      <div style={{marginBottom: 10}}>
+        <h1 style={styles.title}>{msg.animationPicker_title()}</h1>
+        {!is13Plus && !hideUploadOption && (
+          <WarningLabel>{msg.animationPicker_warning()}</WarningLabel>
+        )}
+        <SearchBar
+          placeholderText={msg.animationSearchPlaceholder()}
+          onChange={evt => this.onSearchQueryChange(evt.target.value)}
+        />
+        {(searchQuery !== '' || categoryQuery !== '') && (
+          <div style={animationPickerStyles.navigation}>
+            {categoryQuery !== '' && (
+              <div style={animationPickerStyles.breadCrumbs}>
+                {this.props.navigable && (
+                  <span
+                    onClick={this.onClearCategories}
+                    style={animationPickerStyles.allAnimations}
+                  >
+                    {`${msg.animationPicker_allCategories()} > `}
+                  </span>
+                )}
+                <span>{msg[`animationCategory_${categoryQuery}`]()}</span>
+              </div>
+            )}
+          </div>
+        )}
+        <ScrollableList
+          style={{maxHeight: MAX_HEIGHT}}
+          onScroll={this.handleScroll}
+        >
+          {(searchQuery !== '' || categoryQuery !== '') &&
+            results.length === 0 && (
+              <div style={animationPickerStyles.emptyResults}>
+                {msg.animationPicker_noResultsFound()}
+              </div>
+            )}
+          {((searchQuery === '' && categoryQuery === '') ||
+            (results.length === 0 && this.props.canDraw)) && (
+            <div>
+              <AnimationPickerListItem
+                label={msg.animationPicker_drawYourOwn()}
+                icon="pencil"
+                onClick={this.props.onPickNewAnimation}
+              />
+              {!hideUploadOption && (
+                <AnimationPickerListItem
+                  label={msg.animationPicker_uploadImage()}
+                  icon="upload"
+                  onClick={this.onUploadClick}
+                />
+              )}
+            </div>
+          )}
+          {searchQuery === '' &&
+            categoryQuery === '' &&
+            this.animationCategoriesRendering()}
+          {(searchQuery !== '' || categoryQuery !== '') &&
+            this.animationItemsRendering(results || [])}
+        </ScrollableList>
+      </div>
     );
   }
 
@@ -160,3 +380,10 @@ export default connect(
     }
   })
 )(AnimationPicker);
+
+const WarningLabel = ({children}) => (
+  <span style={{color: color.red}}>{children}</span>
+);
+WarningLabel.propTypes = {
+  children: PropTypes.node
+};
